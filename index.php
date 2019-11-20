2dbedf1a17bc0f7a799ed814e7cacaaea206835b
kigupta5 <kigupta001@gmail.com>
2019-11-20 18:16:19 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+    <?php echo "<p>Hello World! Here are all of my links [Khushi Gupta Period 2]</p>"; ?>
+    <ul>
+      <li><a href="Gupta_index.html">Home</a></li>
+      <li><a href="Gupta_AboutMe.html">AboutMe</a></li>
+      <li><a href="Gupta_Projects.html">Projects</a></li>
+      <li><a href="Gupta_Resume.html">Resume</a></li>
+      <li><a href="Gupta_WorkExperience.html">Work Experience</a></li>
+      <li><a href="Gupta_ContactMe.html">ContactMe</a></li>
+    </ul>
+  </body>
+</html>
+<!--git init
+git remote add origin https://github.com/kigupta5/portfolio
+git config --global user.email 'kigupta001@gmail.com'
+git config --global user.name 'kigupta5'
+git add .
+git commit -m 'test'
+git push - u origin master
+git remote add origin https://github.com/kigupta5/portfolio.git-->
